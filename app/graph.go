de5208a925155d5206fef7f7cdb578e787165bd3
horry <horry@pop-os.localdomain>
2024-04-18 20:38:15 +0200

    now can tab trough examples

--- app/graph.go
+++ app/graph.go
@@ -20,6 +20,18 @@ const (
 	HORIZONTAL
 )
 
+var (
+	current_container = 0
+	containers        = []Node{container.New(VERTICAL, lipgloss.NewStyle(), MakeGrid(
+		func(x int, y int) int { return int(math.Tan(float64(x) * float64(y))) })),
+		container.New(VERTICAL, lipgloss.NewStyle(), MakeGrid(
+			func(x int, y int) int { return x * y })),
+		container.New(VERTICAL,
+			lipgloss.NewStyle().Border(lipgloss.RoundedBorder()),
+			[]Node{text.New("ligma balls", lipgloss.NewStyle())}),
+	}
+)
+
 type Node interface {
 	Init() tea.Cmd
 	Children() []Node
@@ -31,22 +43,26 @@ type Node interface {
 type NodeGraph struct {
 	Graph Node
 }
+type calcXY func(x int, y int) int
 
-func DefaultNodeGraph() NodeGraph {
-
+func MakeGrid(fn calcXY) []Node {
 	var cols []Node
 	for x := 0; x < 20; x++ {
 		var texts []Node
 		for y := 0; y < 40; y++ {
 			s := lipgloss.NewStyle().
 				Foreground(lipgloss.
-					Color(strconv.Itoa(int(math.Tan(float64(x) * float64(y))))))
+					Color(strconv.Itoa(fn(x, y))))
 			texts = append(texts, text.New("█", s))
 
 		}
 		cols = append(cols, container.New(HORIZONTAL, lipgloss.NewStyle(), texts))
 	}
-	c := container.New(VERTICAL, lipgloss.NewStyle(), cols)
+	return cols
+}
+
+func DefaultNodeGraph() NodeGraph {
+
 	header := text.New("20x40 grid forground color = math.Tan(x * y)", lipgloss.NewStyle().
 		Bold(true))
 	footer := text.New("made with bubbletea!", lipgloss.NewStyle().
@@ -57,7 +73,7 @@ func DefaultNodeGraph() NodeGraph {
 
 	return NodeGraph{
 		Graph: Container{
-			nodes: []Node{header, c, footer},
+			nodes: []Node{header, containers[2], footer},
 			join:  VERTICAL,
 			style: lipgloss.NewStyle().Align(lipgloss.Center),
 		}}
@@ -76,6 +92,15 @@ func (ng NodeGraph) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "esc", "ctrl+c":
 			return ng, tea.Quit
+		case "tab":
+			switch ng.Graph.(type) {
+			case Container:
+				if current_container == len(containers) {
+					current_container = 0
+				}
+				ng.Graph.(Container).nodes[1] = containers[current_container]
+				current_container++
+			}
 		}
 	}
 
